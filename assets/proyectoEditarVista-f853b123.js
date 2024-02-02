aff1cdbcdd60fa83c677f4f3dcf5e9eb9e948451
daw2-barrio23 <barriomunoznicolas@fpllefia.com>
2024-02-02 16:06:28 +0100

    Updates

--- assets/proyectoEditarVista-f853b123.js
+++ assets/proyectoEditarVista-f853b123.js
@@ -1,4 +1,3 @@
-import { p as proyectos } from "./datosPrueba-ef04ddb8.js";
 const proyectoEditarVista = {
   template: (
     // html
@@ -6,14 +5,15 @@ const proyectoEditarVista = {
   <div class="container">
   <h1 class="mt-5">Edición de proyecto</h1>
   <div class="d-flex justify-content-end">
-    <bottom id="botonVolver" class="btn btn-outline-secondary mt-5 bi bi-arrow-bar-left router-link">
+    <bottom class="btn btn-outline-secondary mt-5">
+      <i class="bi bi-arrow-bar-left" style="font-size: 1em"></i>
       Volver</bottom
     >
   </div>
-  <form novalidate id="formularioEditarProyecto" action="" class="form">
+  <form novalidate id="formulario" action="" class="">
     <div class="row mt-2">
       <div class="col-12 col-md-4 pt-2 mb-3">
-        <img id="imagenJuego" src="images/juego.jpg" alt="" class="img-fluid" />
+        <img src="/assets/images/juego.jpg" alt="" class="img-fluid" />
         <label class="form-label mt-2" for="urlImagen"
           ><strong>URL imagen: </strong></label
         >
@@ -34,7 +34,7 @@ const proyectoEditarVista = {
         <label class="form-label" for="nombre"><strong>Nombre: </strong></label>
         <input
           required
-          id="nombreJuego"
+          id="nombre"
           type="text"
           value="Nombre Autor"
           class="form-control"
@@ -56,8 +56,8 @@ const proyectoEditarVista = {
           ><strong>Estado: </strong></label
         >
         <select required id="estado" class="form-control">
-          <option value="Cerrado">Cerrado</option>  
-          <option value="En desarrollo">En desarrollo</option>
+          <option value="estado">estado</option>
+          <option value="otro estado">otro estado</option>
         </select>
         <div class="invalid-feedback">
           Debes definir un estado
@@ -67,7 +67,7 @@ const proyectoEditarVista = {
         <label class="form-label mt-2" for="fecha"
           ><strong>Fecha: </strong></label
         >
-        <input id="fecha" type="date" class="form-control"  />
+        <input id="fecha" type="date" class="form-control" value="12/12/2023" />
         <div class="invalid-feedback">
           El formato no es correcto
         </div>
@@ -99,10 +99,9 @@ const proyectoEditarVista = {
 
         <!-- Submit -->
         <input
-          id="subirProyecto"
           type="submit"
           class="btn btn-success mt-3"
-          value="Actualizar proyecto"
+          value="Subir proyecto"
         />
       </div>
     </div>
@@ -110,50 +109,16 @@ const proyectoEditarVista = {
 </div>
   `
   ),
-  script: (id) => {
-    const proyectoArray = proyectos.filter((p) => p.id == id);
-    const proyecto = proyectoArray[0];
-    const fecha = proyecto.created_at;
-    const fechaCorta = fecha.split("T")[0];
-    document.querySelector("#imagenJuego").setAttribute("src", proyecto.imagen);
-    document.querySelector("#urlImagen").value = proyecto.imagen;
-    document.querySelector("#nombreJuego").value = proyecto.nombre;
-    document.querySelector("#descripcion").value = proyecto.descripcion;
-    document.querySelector("#estado").value = proyecto.estado;
-    document.querySelector("#fecha").value = fechaCorta;
-    console.log(fechaCorta);
-    document.querySelector("#enlace").value = proyecto.enlace;
-    document.querySelector("#repositorio").value = proyecto.repositorio;
-    document.querySelector("#botonVolver").addEventListener("click", () => {
-      window.history.back();
-    });
-    const inputUrl = document.querySelector("#urlImagen");
-    inputUrl.addEventListener("input", () => {
-      const imagen = document.querySelector("#imagenJuego");
-      imagen.setAttribute("src", inputUrl.value);
-    });
-    const formulario = document.querySelector("#formularioEditarProyecto");
+  script: () => {
+    console.log("vista proyectoDetalleEditar cargada");
+    const formulario = document.querySelector("#formulario");
     formulario.addEventListener("submit", (event) => {
-      event.preventDefault();
-      event.stopPropagation();
       if (!formulario.checkValidity()) {
-        formulario.classList.add("was-validated");
-      } else {
-        enviaDatos();
+        event.preventDefault();
+        event.stopPropagation();
       }
+      formulario.classList.add("was-validated");
     });
-    function enviaDatos() {
-      const proyectoEditado = {
-        imagen: document.querySelector("#urlImagen").value,
-        nombre: document.querySelector("#nombreJuego").value,
-        descripcion: document.querySelector("#descripcion").value,
-        estado: document.querySelector("#estado").value,
-        enlace: document.querySelector("#enlace").value,
-        repositorio: document.querySelector("#repositorio").value
-      };
-      alert(`Enviando a la base de datos el objeto con id = ${proyecto.id}`);
-      console.log(`Enviando a la base de datos el objeto con id = ${proyecto.id}`, proyectoEditado);
-    }
   }
 };
 export {
